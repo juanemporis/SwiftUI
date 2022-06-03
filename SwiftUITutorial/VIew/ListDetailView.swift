c69c628a20560a01ff1cb44057078c0ce500c0df
juan castañeda <juanemporis123>
2022-06-03 17:22:00 -0500

    Se agrego en Binding var favorite en el archivo ListDetailView , también se agrego un HStack con un botón y un icono de estrella para saber si el programador o programadora es favorito o no. En el archivo ListView se añadió una clase ProgrammerModelData: ObservableObject, en la variable programemos se agrego un @Published, se agrego un  @EnvironmentObject var programarsModelData y se modifico la ubicación del navigationView y del navitagionTitle

--- SwiftUITutorial/VIew/ListDetailView.swift
+++ SwiftUITutorial/VIew/ListDetailView.swift
@@ -2,23 +2,34 @@
 //  ListDetailView.swift
 //  SwiftUITutorial
 //
-//  Created by wendy manrique on 2/06/22.
+
 //
 
 import SwiftUI
 
 struct ListDetailView: View {
-    
+    //NOTA:El binding es una variable que a llegado de otra clase a esta y puede ser modificada
     var programmer : Programmer
-        
+    @Binding var favorite: Bool
     
     var body: some View {
-        
         //Se creo un avatar para el programador con sus distintos cambios y se modifico la imagen a circular y dandole un color negro y los radios.
         //Se hizo el llamado de los datos del programador y su lenguaje que maneja y tmb se agrego un spacer 
         VStack {
             programmer.avatar.resizable().frame(width: 200, height: 200).clipShape(Circle()).overlay(Circle().stroke(Color .black,lineWidth: 4)).shadow(color: Color.gray, radius: 5)
+            
+            HStack {
             Text(programmer.name).font(.largeTitle)
+            Button {
+                favorite.toggle() //Esta accion cambiara el estado de true a false o false a true
+            } label: {
+                if favorite { //Si es favorito se marcara como estrella amarilla
+                    Image(systemName: "star.fill").foregroundColor(.yellow)
+                }else{
+                Image(systemName: "star").foregroundColor(.black)//Si es false no se marcara como una estrella amarilla si no de color negro
+                }
+            }
+        }
             Text(programmer.languages).font(.title)
             Spacer()
         }
@@ -27,6 +38,6 @@ struct ListDetailView: View {
 
 struct ListDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        ListDetailView(programmer: Programmer(id: 1, name: "Juan Castañeda", languages: "Swift,Kotlin", avatar: Image(systemName: "person.fill"),favorite: true))
+        ListDetailView(programmer: Programmer(id: 1, name: "Juan Castañeda", languages: "Swift,Kotlin", avatar: Image(systemName: "person.fill"),favorite: true), favorite: .constant(false))
     }
 }
--- SwiftUITutorial/VIew/ListView.swift
+++ SwiftUITutorial/VIew/ListView.swift
@@ -7,47 +7,52 @@
 
 import SwiftUI
 
+final class ProgrammersModelData: ObservableObject {
+
 //Se crea private let programador llamando a la struct Programmer creada y se agregan 4 programadores mas con sus nombre y lenguaje de programacion y el avatar destinado
-private let programmers = [Programmer(id: 1, name: "Juan Castañeda", languages: "Swift, Kotlin", avatar: Image(systemName: "person"), favorite: true),
-                           Programmer(id: 2, name: "Ana", languages: "Java, Kotlin", avatar: Image(systemName: "person.fill"), favorite: false),
-                           Programmer(id: 3, name: "Julio", languages: "C++, C#", avatar: Image(systemName: "person.fill"), favorite: false),
-                           Programmer(id: 4, name: "Pablo", languages: "JavaScript", avatar: Image(systemName: "person.fill"), favorite: true),
-                           Programmer(id: 5, name: "Sara", languages: "Go, Python", avatar: Image(systemName: "person.fill"), favorite: false)]
+    @Published var programmers = [Programmer(id: 0, name: "Juan Castañeda", languages: "Swift, Kotlin", avatar: Image(systemName: "person"), favorite: true),
+                           Programmer(id: 1, name: "Ana", languages: "Java, Kotlin", avatar: Image(systemName: "person.fill"), favorite: false),
+                           Programmer(id: 2, name: "Julio", languages: "C++, C#", avatar: Image(systemName: "person.fill"), favorite: false),
+                           Programmer(id: 3, name: "Pablo", languages: "JavaScript", avatar: Image(systemName: "person.fill"), favorite: true),
+                           Programmer(id: 4, name: "Sara", languages: "Go, Python", avatar: Image(systemName: "person.fill"), favorite: false)]
+}
 
 //Se crea la lista programador agregado el RowView creado 
 struct ListView: View {
+    
+    @EnvironmentObject var programmersModelData: ProgrammersModelData
     
     //Se agrego una variable  showFavorites con un booleano false
     @State private var showFavorites = false
     
     //Se agrego una variable filteredProgrammer para filtrar el listado de los programadores y programadoras
     private var filteredProgrammers: [Programmer] {
-        return programmers.filter { Programmer in
+        return programmersModelData.programmers.filter { Programmer in
             return !showFavorites || Programmer.favorite
             
         }
     }
     
     var body: some View {
+        
+        NavigationView{//Se agrego un navigationView con el nombre de programmers para poder trasladar los datos al darle click en cada nombre
+        
         //Se agrego un VStack y una barra Toggle y un nombre "Mostrar favoritos" para realizar un cambio de estado
         VStack {
             Toggle(isOn: $showFavorites) {
                 Text("Mostrar favoritos")
             }.padding()
         
-        
-        //Se agrego un navigationView con el nombre de programmers para poder trasladar los datos al darle click en cada nombre
-        NavigationView{
-            
+    
         List(filteredProgrammers,id: \.id) { programmer in
-            NavigationLink(destination: ListDetailView(programmer: programmer)){
+            
+            NavigationLink(destination: ListDetailView(programmer: programmer, favorite: $programmersModelData.programmers[programmer.id].favorite)){
             RowView(programmer: programmer)
                 
             }
           }
             //Se agrego el encabezado nombre de Programmers
-        .navigationTitle("Programmers")
-          }
+          }.navigationTitle("Programmers")
         }
     }
 }
@@ -55,7 +60,7 @@ struct ListView: View {
 //Se da el llamado al ListView con los datos de los programadores
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
-        ListView()
+        ListView().environmentObject(ProgrammersModelData())
     }
 }
 
